01e299fefc5b0a265804d35ff479c8408fe1d4dc
00Greenwood <37928119+00Greenwood@users.noreply.github.com>
2022-12-03 07:52:29 -0500

    Solve Day 3

---
+++ src/Day3.ts
@@ -0,0 +1,60 @@
+import { Day } from "./Day";
+
+interface Rucksack {
+  first: number[];
+  second: number[];
+  total: number[];
+}
+
+export class Day3 extends Day {
+  constructor() {
+    super("Day3", "Day 3");
+  }
+
+  private convertFromUnicode(input: number): number {
+    if (input >= 97 && input <= 122) {
+      return input - 96;
+    }
+    return input - 38;
+  }
+
+  private parseInput(input: string): Rucksack[] {
+    return input.split("\n").map<Rucksack>((line) => {
+      const middle = line.length / 2;
+      const rucksack: Rucksack = { first: [], second: [], total: [] };
+      for (let index = 0; index < middle; index++) {
+        const value = this.convertFromUnicode(line.charCodeAt(index));
+        rucksack.first.push(value);
+        rucksack.total.push(value);
+      }
+      for (let index = middle; index < line.length; index++) {
+        const value = this.convertFromUnicode(line.charCodeAt(index));
+        rucksack.second.push(value);
+        rucksack.total.push(value);
+      }
+      return rucksack;
+    });
+  }
+
+  public async solvePartOne(input: string): Promise<string> {
+    const rucksacks = this.parseInput(input);
+    const result = rucksacks.map(({ first, second }) => {
+      return first.filter((value) => second.includes(value))[0];
+    });
+    return result.reduce((score, value) => score + value, 0).toString();
+  }
+
+  public async solvePartTwo(input: string): Promise<string> {
+    const rucksacks = this.parseInput(input);
+    const result = new Array<number>();
+    for (let index = 0; index < rucksacks.length; index += 3) {
+      const first = rucksacks[index].total;
+      const second = rucksacks[index + 1].total;
+      const third = rucksacks[index + 2].total;
+      const common = first.filter((value) => second.includes(value));
+      const badge = common.filter((value) => third.includes(value));
+      result.push(badge[0]);
+    }
+    return result.reduce((score, value) => score + value, 0).toString();
+  }
+}
--- src/solve.ts
+++ src/solve.ts
@@ -3,6 +3,7 @@ import { Day } from "./Day";
 import { Day0 } from "./Day0";
 import { Day1 } from "./Day1";
 import { Day2 } from "./Day2";
+import { Day3 } from "./Day3";
 
 const parser = new ArgumentParser({
   description: "Solver for Advent of Code 2022!",
@@ -17,6 +18,7 @@ const days = new Array<Day>();
 days.push(new Day0());
 days.push(new Day1());
 days.push(new Day2());
+days.push(new Day3());
 
 const args = parser.parse_args();
 
@@ -24,6 +26,7 @@ switch (args.day) {
   case 0:
   case 1:
   case 2:
+  case 3:
     days[args.day].solve();
     break;
   default:
