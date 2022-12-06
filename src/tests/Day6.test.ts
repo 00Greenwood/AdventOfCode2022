c3d9cabb5f9d25472be558f7a92f5060f3d6c13f
00Greenwood <37928119+00Greenwood@users.noreply.github.com>
2022-12-06 07:56:09 -0500

    Solve Day 6

---
+++ src/tests/Day6.test.ts
@@ -0,0 +1,19 @@
+import { Day6 } from "../days/Day6";
+
+describe("Day 6", () => {
+  const day = new Day6();
+
+  test("Name", async () => {
+    expect(day.name).toBe("Day6");
+  });
+
+  test("Part 1", async () => {
+    expect(await day.solvePartOne(day.testInput)).toBe(11);
+    expect(await day.solvePartOne(day.input)).toBe(1080);
+  });
+
+  test("Part 2", async () => {
+    expect(await day.solvePartTwo(day.testInput)).toBe(26);
+    expect(await day.solvePartTwo(day.input)).toBe(3645);
+  });
+});
